4be1ca5079989fd122f3dba0e469935cb750039f
Mostafa <mos.sameti@gmail.com>
2020-05-04 18:45:09 +0430

    add focus management logic to main component

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 import Input from './components/input';
 import SubmitButton from './components/submit-button';
@@ -11,20 +11,33 @@ export default function App(params) {
     expirationMonth: '12',
     expirationYear: '21',
   });
+  const [focusedField, setFocusedField] = useState("");
   const handleCardInfoChange = (e, fieldName) => {
     setCard({ ...card, [fieldName]: e.target.value });
+  };
+
+  const fieldRefs = {};
+  Object.keys(card).map((field) => {
+    fieldRefs[field] = useRef(null);
+  });
+
+  const handleFocus = (fieldName) => {
+    fieldRefs[fieldName].current.focus();
   };
 
   return (
     <div>
       <CardView
         cardInfo={card}
+        focusOnInput={handleFocus}
+        focusedField={focusedField}
       />
       {Object.keys(card).map((fieldName) => {
         return (
           <Input
             key={fieldName}
             name={fieldName}
+            ref={fieldRefs[fieldName]}
             onChange={(e) => handleCardInfoChange(e, fieldName)}
             value={card[fieldName]}
             onFocus={setFocusedField}
