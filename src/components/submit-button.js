1cf5575f2dd2fb17dcdb02453ecacc0252da0c8e
Mostafa <mos.sameti@gmail.com>
2020-05-05 14:49:06 +0430

    add submit button styles

---
+++ src/components/submit-button.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function SubmitButton({ text, onClick }) {
+  return <button className="submit-button" onClick={onClick}>{text}</button>;
+}
+
+SubmitButton.defaultProps = {
+  text: "Submit"
+};
